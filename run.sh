926fd8b47db358665cf9201056aafbb5c473efa9
Adam Lukens <alukens@purduefed.com>
2016-03-27 03:35:37 -0400

    Clean up the shell script a bit
    This should run the right file regardless of home directory set up.

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ if hash foo 2>/dev/null; then
   eval "$(rbenv init -)"
 fi
 
-cd /Users/adam/software/programming/rubomatic
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPT")
+cd $SCRIPTDIR
 bundle exec ruby rubomatic.rb
